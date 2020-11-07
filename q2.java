cc2585339385930a62260404cf4336d957ee9520
Mohit Sahoo <73797451+BlackFanng@users.noreply.github.com>
2020-11-08 01:50:30 +0530

    Update q2.java
    Made some changes in the code.

--- q2.java
+++ q2.java
@@ -1,75 +1,98 @@
 package raceToQualify;
 import java.util.*;
-public class q2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-Scanner sc=new Scanner(System.in);
-System.out.println("Enter User's choice");
-//Use 0-Rock, 1-Paper, @-Scissor
-int n=sc.nextInt();
-int comp=(int)((n*Math.random()));
-sc.close();
-switch(n)
+public class q2
 {
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the number of test cases ");
+		int t=sc.nextInt();
+		int count=0;
+		int n;
 
-	case 0:
-		System.out.println("Your response is R");
-		if(comp==0)
+		for(int i=0; i<t; i++)
 		{
-			System.out.println("Computer's response is R. It's a TIE!");
+			
+			System.out.print("Enter your response. 0=R, 1=P, 2=S ");
+			n=sc.nextInt();
+			int comp= (int)(n*Math.random());
+			
+			switch(n)
+			{
+				case 0:
+					System.out.println("Your response is R");
+					if(comp==0)
+					{
+						System.out.println("Computer's response is R. It's a TIE!");
+					}
+					
+					if(comp==1)
+					{
+						System.out.println("Computer's response is P. You LOSE!");
+					}
+					
+					if(comp==2)
+					{
+						System.out.println("Computer's response is S. You WIN!");
+						count=count+1;
+					}
+					break;
+					
+					
+				case 1:
+					System.out.println("Your response is P");
+					if(comp==0)
+					{
+						System.out.println("Computer's response is R. You WIN!!");
+						count=count+1;
+					}
+					
+					if(comp==1)
+					{
+						System.out.println("Computer's response is P. It's a TIE!");
+					}
+					
+					if(comp==2)
+					{
+						System.out.println("Computer's response is S. You LOSE!");
+					}
+					break;
+					
+					
+				case 2:
+					System.out.println("Your response is S");
+					if(comp==0)
+					{
+						System.out.println("Computer's response is R. You LOSE!");
+					}
+					
+					if(comp==1)
+					{
+						System.out.println("Computer's response is P. You WIN!");
+						count=count+1;
+					}
+					
+					if(comp==2)
+					{
+						System.out.println("Computer's response is S. It's a TIE!");
+					}
+					break;
+					
+					default:
+						System.out.println("Invalid response!");
+			}
 		}
-		
-		if(comp==1)
-		{
-			System.out.println("Computer's response is P. You LOSE!");
-		}
-		
-		if(comp==2)
-		{
-			System.out.println("Computer's response is S. You WIN!");
-		}
-		break;
-		
-		
-	case 1:
-		System.out.println("Your response is P");
-		if(comp==0)
-		{
-			System.out.println("Computer's response is R. You WIN!!");
-		}
-		
-		if(comp==1)
-		{
-			System.out.println("Computer's response is P. It's a TIE!!");
-		}
-		
-		if(comp==2)
-		{
-			System.out.println("Computer's response is S. You LOSE!");
-		}	
-		break;
-		
-	case 2:	
-		System.out.println("Your response is S");
-		if(comp==0)
-		{
-			System.out.println("Computer's response is R. You LOSE!");
-		}
-		
-		if(comp==1)
-		{
-			System.out.println("Computer's response is P. You WIN!");
-		}
-		
-		if(comp==2)
-		{
-			System.out.println("Computer's response is S. It's a TIE!");
-		}
-		break;
+if(count>t/2 && count<=t)
+{
+	System.out.println(t+ " rounds over. User scored " +count+ " .User won the game");
+}
+else if(count==t/2)
+	System.out.println(t+ " rounds over. User scored " +count+ " .It's a tie.");
 
+else
+{
+	System.out.println(t+ " rounds over. User scored " +count+ " .User won the game");
 }
 	}
-
-
 }
